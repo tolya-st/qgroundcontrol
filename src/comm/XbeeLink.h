751bfc0cb3ea47c0e0250e28f910c7f017f24474
Lorenz Meier <lm@inf.ethz.ch>
2013-09-02 11:24:09 +0200

    Compile fixes for XBee Link

--- src/comm/XbeeLink.h
+++ src/comm/XbeeLink.h
@@ -22,7 +22,7 @@ public:
 public: // virtual functions from XbeeLinkInterface
 	QString getPortName() const;
 	void requestReset() { }
-	int getBaudRate();
+	int getBaudRate() const;
 
 public slots: // virtual functions from XbeeLinkInterface
 	bool setPortName(QString portName);
